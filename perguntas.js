689b72fc070dded753bb266c7a28f5a4040037a9
NickMaia17 <00001086130054SP@al.educacao.sp.gov.br>
2024-10-06 15:34:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Nicolas',
+    'O nicolas é Viado?',
+    'Sim Concerteza'
+)
+
+criaCartao(
+    'Nicolas',
+    'o Nicolas dá a Bunda?',
+    'sempre que tem tempo'
+)
+
+criaCartao(
+    'Nicolas',
+    'qual o tamanho do cu do nicolas?',
+    'imenso do tamanho do universo'
+)
+
+criaCartao(
+    'Nicolas',
+    'o nicolas gosta de pica?',
+    'sim e nao é pouco'
+)
